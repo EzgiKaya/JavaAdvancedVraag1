f3a9c7966f66e0388748946990e8bb8f58ca6b07
Ezgi Kaya <ezgi.kaya@student.ap.be>
2018-04-18 14:22:47 +0200

    Unique answers till answerpool is exhausted

--- SPRING-EXAMEN-JAVA2-VRAAG1-START/src/main/java/edu/ap/spring/model/EightBall.java
+++ SPRING-EXAMEN-JAVA2-VRAAG1-START/src/main/java/edu/ap/spring/model/EightBall.java
@@ -1,9 +1,10 @@
 package edu.ap.spring.model;
+import edu.ap.spring.jpa.Question;
 import edu.ap.spring.jpa.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Random;
+import java.util.*;
 
 @Component
 public class EightBall {
@@ -19,13 +20,46 @@ public class EightBall {
 								"Cannot predict now",
 								"Don't count on it", 
 								"Outlook not so good"};
+
+	private List<String> givenAnswers = new ArrayList<String>() ;
 	
 	public String getRandomAnswer(String question) {
 		String answer = "";
-		Random random = new Random();
-		answer = answers[random.nextInt(answer.length()) - 1];
 
-		return answer;
+		//Check if the question has been asked before
+		Question foundQuestion = repository.findByQuestion(question);
+
+		// If not, give a new random answer
+		if(foundQuestion == null) {
+		    // Initialise the random class
+            Random random = new Random();
+
+            // Get a random answer from our pool
+            answer = answers[random.nextInt(answers.length)];
+            while(givenAnswers != null && givenAnswers.contains(answer)){
+                answer = answers[random.nextInt(answers.length)];
+            }
+
+            // Add the answer to the given answer pool
+            givenAnswers.add(answer);
+
+            // reset the given pool if full
+            if(givenAnswers.size() == answers.length){
+                givenAnswers = new ArrayList<String>();
+            }
+
+            //Save the question + answer
+            Question newQuestion = new Question(question, answer);
+            repository.save(newQuestion);
+
+            // Remove the answer from the pool and reset the pool if it's empty
+
+            return answer;
+        }
+        else{
+		    // Return the answer for the question which has been asked already
+		    return foundQuestion.getAnswer();
+        }
 	}
 
 	public String[] getAnswers() {
