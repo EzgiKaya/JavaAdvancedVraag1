202951be59c08632bc00c91100e0de453ed9a988
Ezgi Kaya <ezgi.kaya@student.ap.be>
2018-04-18 14:52:10 +0200

    EightBall UI development

--- SPRING-EXAMEN-JAVA2-VRAAG1-START/src/main/java/edu/ap/spring/view/UI.java
+++ SPRING-EXAMEN-JAVA2-VRAAG1-START/src/main/java/edu/ap/spring/view/UI.java
@@ -1,13 +1,66 @@
 package edu.ap.spring.view;
 
+import edu.ap.spring.model.EightBall;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 @Component
 public class UI implements InitializingBean {
-	
+
+    @Autowired
+    EightBall eightBall;
+
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		System.setProperty("java.awt.headless", "false");
 	}
+
+	private JFrame jFrame;
+	private JLabel questionLabel, answer;
+	private JTextField questionText;
+	private JButton btnGetAnswer;
+
+
+	public UI() {}
+
+	public void setupUI(){
+	    jFrame = new JFrame("The Magnificent Eight-Ball");
+	    jFrame.setLayout(new FlowLayout());
+
+	    questionLabel = new JLabel("Ask your question mortal :");
+	    questionText = new JTextField(15);
+
+        btnGetAnswer = new JButton();
+	    btnGetAnswer.setText("What do you think o mighty Eight Ball?");
+	    btnGetAnswer.addActionListener(new ActionListener(){
+	        public void actionPerformed(ActionEvent e){
+                if( getQuestion().getText() != "" ){
+                    setAnswer(eightBall.getRandomAnswer(getQuestion().getText()));
+                }
+            }
+        });
+
+
+	    jFrame.setSize(400,400);
+	    jFrame.setLocationRelativeTo(null);
+	    jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    jFrame.pack();
+	    jFrame.setVisible(true);
+    }
+
+    public JFrame getjFrame() {
+        return this.jFrame;
+    }
+
+    public JTextField getQuestion() { return this.questionText ;}
+
+    public void setAnswer(String answer){
+	    this.answer.setText(answer);
+    }
+
 }
