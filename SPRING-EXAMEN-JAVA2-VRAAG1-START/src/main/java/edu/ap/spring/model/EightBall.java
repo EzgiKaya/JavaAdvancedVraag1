b7a5e95ede49b0013b6baf263acd549811391b38
Ezgi Kaya <ezgi.kaya@student.ap.be>
2018-04-18 13:31:26 +0200

    8 ball development - repository and random questions

--- SPRING-EXAMEN-JAVA2-VRAAG1-START/src/main/java/edu/ap/spring/model/EightBall.java
+++ SPRING-EXAMEN-JAVA2-VRAAG1-START/src/main/java/edu/ap/spring/model/EightBall.java
@@ -1,8 +1,15 @@
 package edu.ap.spring.model;
+import edu.ap.spring.jpa.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Random;
 
 @Component
 public class EightBall {
+
+    @Autowired
+    private QuestionRepository repository;
 	
 	private String[] answers = {"It is certain", 
 								"Yes definitely", 
@@ -15,6 +22,8 @@ public class EightBall {
 	
 	public String getRandomAnswer(String question) {
 		String answer = "";
+		Random random = new Random();
+		answer = answers[random.nextInt(answer.length()) - 1];
 
 		return answer;
 	}
